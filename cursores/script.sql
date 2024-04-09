8b48b5170e03a2f094e765e06b5f3d37deb8ccef
Rodrigo Bossini <professorbossini@gmail.com>
2024-04-09 10:20:37 -0300

    feat(ap16): encerra secao 2.7
    - mostra exemplo de cursor vinculado

--- cursores/script.sql
+++ cursores/script.sql
@@ -1,24 +1,107 @@
--- um bloquinho anônimo
-DO $$
-DECLARE
-	-- 1. Declaração do cursor (cursor não vinculado)
-	cur_nomes_youtubers REFCURSOR;
-	v_youtuber VARCHAR(200);
-BEGIN
-	-- 2. Abertura do cursor
-	OPEN cur_nomes_youtubers FOR
-		SELECT youtuber FROM tb_youtubers;
+--cursor vinculado (bound)
+--exibir nome de canal concatenado a seu numero de inscritos
+-- mais um bloquinho anônimo
+-- DO $$
+-- 	DECLARE
+-- 		--cursor bound (vinculado)
+-- 		--1. Declaração (ainda não está aberto)
+-- 		cur_nomes_e_inscritos CURSOR FOR
+-- 		SELECT youtuber, subscribers FROM tb_youtubers;
+-- 		tupla RECORD;
+-- 		-- tupla.youtuber me dá o youtuber
+-- 		-- tupla.subscribers me dá o número de subscribers
+-- 		resultado TEXT DEFAULT '';
+-- 	BEGIN
+-- 		--2. Abertura do cursor
+-- 		OPEN cur_nomes_e_inscritos;
+-- 		-- vamos usar um loop WHILE
+-- 		--3. Recuperação de dados
+-- 		FETCH cur_nomes_e_inscritos INTO tupla;
+-- 		WHILE FOUND LOOP
+-- 			--concatenação, operador ||
+-- 			resultado := resultado || tupla.youtuber || ':' || tupla.subscribers || ',';
+-- 			FETCH cur_nomes_e_inscritos INTO tupla;
+-- 		END LOOP;
+-- 		--4. Fechamento
+-- 		CLOSE cur_nomes_e_inscritos;
+-- 		RAISE NOTICE '%', resultado;
+-- 	END;
+-- $$
 
-	--3. Recuperação de dados de interesse
-	LOOP
-		FETCH cur_nomes_youtubers INTO v_youtuber;
-		EXIT WHEN NOT FOUND;
-		RAISE NOTICE '%', v_youtuber;	
-	END LOOP;
-	--4. Fechar o cursor
-	CLOSE cur_nomes_youtubers;
-END;
-$$
+-- -- cursor não vinculado(unbound)
+-- -- cursor om query dinâmica, ou seja, uma query representada
+-- -- como uma string
+-- -- mais um bloquinho anônimo
+-- DO $$
+-- DECLARE
+-- 	--1. Declaração
+-- 	cur_nomes_a_partir_de REFCURSOR;
+-- 	v_youtuber VARCHAR(200);
+-- 	v_ano INT := 2019;
+-- 	v_nome_tabela VARCHAR(200) := 'tb_youtubers';
+-- BEGIN
+-- 	--2. Abertura do cursor
+-- 	OPEN cur_nomes_a_partir_de FOR EXECUTE 
+-- 	format(
+-- 		'
+-- 			SELECT
+-- 				youtuber
+-- 			FROM %s
+-- 			WHERE started >= $1
+-- 		', v_nome_tabela	
+-- 	)USING v_ano;
+-- 	LOOP
+-- 		--3. Recuperação de dados
+-- 		FETCH cur_nomes_a_partir_de INTO v_youtuber;
+-- 		EXIT WHEN NOT FOUND;
+-- 		RAISE NOTICE '%', v_youtuber;
+-- 	END LOOP;
+-- 	--4. Fechamento do cursor
+-- 	CLOSE cur_nomes_a_partir_de;
+-- END;
+-- $$
+
+-- DO $$
+-- DECLARE
+-- 	v_nome VARCHAR(200) := 'Ana';
+-- 	v_saudacao VARCHAR(200) := 'tudo bem?';
+-- 	v_resultado VARCHAR(200);
+-- BEGIN
+-- 	-- Oi, Ana
+-- 	v_resultado := format('Oi, %s. %s', v_nome, v_saudacao);
+-- 	RAISE NOTICE '%', v_resultado;
+-- 	--python: res = f'Oi, {v_nome}. {v_saudacao}';
+-- 	--def exibe_saudacao(nome, saudacao):
+-- 	--	print(f'Oi, {nome}. {saudacao}')
+-- END;
+-- $$
+
+
+
+
+
+
+-- -- um bloquinho anônimo
+-- DO $$
+-- DECLARE
+-- 	-- 1. Declaração do cursor (cursor não vinculado (unbound))
+-- 	cur_nomes_youtubers REFCURSOR;
+-- 	v_youtuber VARCHAR(200);
+-- BEGIN
+-- 	-- 2. Abertura do cursor
+-- 	OPEN cur_nomes_youtubers FOR
+-- 		SELECT youtuber FROM tb_youtubers;
+
+-- 	--3. Recuperação de dados de interesse
+-- 	LOOP
+-- 		FETCH cur_nomes_youtubers INTO v_youtuber;
+-- 		EXIT WHEN NOT FOUND;
+-- 		RAISE NOTICE '%', v_youtuber;	
+-- 	END LOOP;
+-- 	--4. Fechar o cursor
+-- 	CLOSE cur_nomes_youtubers;
+-- END;
+-- $$
 
 
 
